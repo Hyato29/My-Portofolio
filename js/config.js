06dc2cd5983ead0e51cbde738de2ad4628cd2f28
Hyato29 <khayatullahalamin@gmail.com>
2026-07-23 16:31:05 +0800

    fix: config

---
+++ js/config.js
@@ -0,0 +1,4 @@
+const CONFIG = {
+    SUPABASE_URL: 'https://yshoqyqaahutaxukogph.supabase.co',
+    SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlzaG9xeXFhYWh1dGF4dWtvZ3BoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ3ODY5MzIsImV4cCI6MjEwMDM2MjkzMn0.dYuJaDqfNoL-NlmzpRjpDmxwAiGEhbcOg4OUV0fjcJA'
+};
